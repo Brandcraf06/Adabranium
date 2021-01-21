41bb2b5e5f9a0803082dfca76cb2ca0044c0dc5f
Brandcraft06 <Brandcraft06@gmail.com>
2021-01-21 18:40:15 +0100

    Update ModBlocks.java

--- src/main/java/com/brand/adabraniummod/content/ModBlocks.java
+++ src/main/java/com/brand/adabraniummod/content/ModBlocks.java
@@ -7,6 +7,7 @@ import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
+import net.minecraft.block.MaterialColor;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.sound.BlockSoundGroup;
@@ -17,8 +18,8 @@ public class ModBlocks {
 
     public static final Block VIBRANIUM_ORE = register("vibranium_ore", new AdabraniumOreBlock(FabricBlockSettings.of(Material.STONE).strength(6.0f, 300.0f).breakByTool(FabricToolTags.PICKAXES, 3).requiresTool()));
     public static final Block ADAMANTINE_ORE = register("adamantine_ore", new AdabraniumOreBlock(FabricBlockSettings.of(Material.STONE).strength(25.0f, 300.0f).breakByTool(FabricToolTags.PICKAXES, 3).requiresTool()));
-    public static final Block VIBRANIUM_BLOCK = register("vibranium_block", new Block(FabricBlockSettings.of(Material.METAL).strength(4.0f, 600.0f).breakByTool(FabricToolTags.PICKAXES, 3).requiresTool()));
-    public static final Block ADAMANTIUM_BLOCK = register("adamantium_block", new Block(FabricBlockSettings.of(Material.METAL).strength(50.0f, 1200.0f).breakByTool(FabricToolTags.PICKAXES, 3).requiresTool()));
+    public static final Block VIBRANIUM_BLOCK = register("vibranium_block", new Block(FabricBlockSettings.of(Material.METAL, MaterialColor.PURPLE).strength(4.0f, 600.0f).breakByTool(FabricToolTags.PICKAXES, 3).requiresTool()));
+    public static final Block ADAMANTIUM_BLOCK = register("adamantium_block", new Block(FabricBlockSettings.of(Material.METAL, MaterialColor.GREEN).strength(50.0f, 1200.0f).breakByTool(FabricToolTags.PICKAXES, 3).requiresTool()));
     public static final Block HEART_SHAPED_PLANT = register("heart_shaped_plant", new HeartShapedPlantBlock(FabricBlockSettings.of(Material.PLANT).strength(0).sounds(BlockSoundGroup.GRASS).luminance(10).ticksRandomly().noCollision().breakInstantly()));
 
     public static Block register(String id, Block block, boolean registerItem) {
