8768c2fa6bc2fb922129cb43540d2f6405c34f32
Brandcraft06 <Brandcraft06@gmail.com>
2021-04-18 14:46:42 +0200

    Update AdabraniumConfiguredFeatures.java

--- src/main/java/com/brand/adabraniummod/world/AdabraniumConfiguredFeatures.java
+++ src/main/java/com/brand/adabraniummod/world/AdabraniumConfiguredFeatures.java
@@ -15,8 +15,6 @@ import net.minecraft.world.gen.GenerationStep;
 import net.minecraft.world.gen.YOffset;
 import net.minecraft.world.gen.decorator.CountExtraDecoratorConfig;
 import net.minecraft.world.gen.decorator.Decorator;
-import net.minecraft.world.gen.decorator.DepthAverageDecoratorConfig;
-import net.minecraft.world.gen.decorator.RangeDecoratorConfig;
 import net.minecraft.world.gen.feature.*;
 import net.minecraft.world.gen.stateprovider.SimpleBlockStateProvider;
 
