1cbe4e3ea4b6fdff8bf7ca4aff807f7a738432e0
Brandcraft06 <Brandcraft06@gmail.com>
2023-01-29 01:23:19 +0100

    Wrong parameters

--- src/main/java/com/brand/adabranium/content/stuff/types/NetherStuff.java
+++ src/main/java/com/brand/adabranium/content/stuff/types/NetherStuff.java
@@ -30,9 +30,9 @@ public class NetherStuff {
         this.leggings = ModItems.register(type + "_leggings", new ArmorItem(AdabraniumArmorMaterials.NETHER_BRICK, EquipmentSlot.LEGS, (new Item.Settings().fireproof())));
         this.boots = ModItems.register(type + "_boots", new ArmorItem(AdabraniumArmorMaterials.NETHER_BRICK, EquipmentSlot.FEET, (new Item.Settings().fireproof())));
         this.shovel = ModItems.register(type + "_shovel", new BaseShovel(AdabraniumToolMaterials.NETHER_BRICK, 1.5F, -3.0F, (new Item.Settings().fireproof())));
-        this.axe = ModItems.register(type + "_axe", new BaseAxe(AdabraniumToolMaterials.NETHER_BRICK, 5.0F, -3.0F, (new Item.Settings().fireproof())));
+        this.axe = ModItems.register(type + "_axe", new BaseAxe(AdabraniumToolMaterials.NETHER_BRICK, 6.0F, -3.1F, (new Item.Settings().fireproof())));
         this.pickaxe = ModItems.register(type + "_pickaxe", new BasePickaxe(AdabraniumToolMaterials.NETHER_BRICK, 1, -2.8F, (new Item.Settings().fireproof())));
-        this.hoe = ModItems.register(type + "_hoe", new BaseHoe(AdabraniumToolMaterials.NETHER_BRICK, -2, -0.0F, (new Item.Settings().fireproof())));
+        this.hoe = ModItems.register(type + "_hoe", new BaseHoe(AdabraniumToolMaterials.NETHER_BRICK, -2, -1.0F, (new Item.Settings().fireproof())));
         this.sword = ModItems.register(type + "_sword", new BaseSword(AdabraniumToolMaterials.NETHER_BRICK, 3, -2.4f, (new Item.Settings().fireproof())));
 
         LIST.add(this);
